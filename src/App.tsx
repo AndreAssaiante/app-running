3932a44400fc246bc0e91cff71ce0e2c81562236
AndreAssaiante <andre.assaiante@gmail.com>
2025-05-27 21:02:15 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useAuthStore } from "./store/authStore";
+import Navigation from "./components/Navigation";
 import Dashboard from "./pages/Dashboard";
 import Onboarding from "./pages/Onboarding";
 
@@ -12,12 +13,16 @@ function App() {
       {!user ? (
         <Onboarding />
       ) : (
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
+        <>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/profile" element={<div>Perfil do Usuário (em breve)</div>} />
+            <Route path="/workouts" element={<div>Lista de Treinos (em breve)</div>} />
+          </Routes>
+        </>
       )}
     </Router>
   );
 }
-
 export default App;
